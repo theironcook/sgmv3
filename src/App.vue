42f948fd8a6446bb842f2f83db365d17650b5dd9
Andrii Mozhlianskyi <amozhl@softserveinc.com>
2021-07-24 18:14:07 +0300

    create Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <navbar />
+    <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+    import { Options, Vue } from 'vue-class-component';
+    import Navbar from '@/components/Navbar.vue';
 
-#nav {
-  padding: 30px;
+    @Options({
+        components: { Navbar }
+    })
+    export default class App extends Vue {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
-  }
-}
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav :class="{'has-shadow': hasScrollOffset}" class="navbar is-white" role="navigation" aria-label="main navigation">
+        <div class="container p-5">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <img src="/logo.png" width="180" height="30">
+                </a>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">Getting started</a>
+                    <a class="navbar-item">Docs</a>
+                    <a class="navbar-item">Pricing</a>
+                    <a class="navbar-item">Blog</a>
+                </div>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-light">Log in</a>
+                            <a class="button is-primary">
+                                <strong>Sign up</strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+    import { Options, Vue } from 'vue-class-component';
+
+    @Options({})
+    export default class Navbar extends Vue {
+        private hasScrollOffset: boolean = false;
+
+        public created (): void {
+            document.addEventListener('scroll', this.onScroll);
+        }
+
+        private onScroll (e: Event): void {
+            this.hasScrollOffset = (<any>e.target).scrollTop > 60;
+        }
+
+        public destroyed () {
+            document.removeEventListener('scroll', this.onScroll);
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .navbar-item {
+        text-transform: uppercase;
+    }
+</style>
