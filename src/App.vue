4b4965e4aefcee1964be851df4c01c723779f5d6
Andrii Mozhlianskyi <amozhl@softserveinc.com>
2021-07-31 14:39:12 +0300

    create layout for intro section

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
 
 <style lang="scss">
     #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
+        font-family: 'Quicksand', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="columns">
-        <div class="column">
-            <h1>Integrate Anything, Automate Everything</h1>
-            <h2>SaaSGlue is a SaaS-based integration, orchestration and automation platform that lets you fill the gaps in your existing automation infrastructure</h2>
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column has-text-left">
+                    <h1 class="title is-2 is-spaced has-text-weight-semibold has-text-grey-darker">
+                        Integrate <span class="deepskyblue">Anything,</span>
+                        <br /> Automate <span class="deepskyblue">Everything</span>
+                    </h1>
+                    <h2 class="subtitle is-5 has-text-weight-medium has-text-grey">SaaSGlue is a SaaS-based integration, orchestration and automation platform that lets you fill the gaps in your existing automation infrastructure.</h2>
+                    <div class="buttons">
+                        <button class="button is-info">Get Started</button>
+                    </div>
+                </div>
+                <div class="column">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/b_7u17gZcX8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
-        <div class="column">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/b_7u17gZcX8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-    </div>
+    </section>
 </template>
 
 <script lang="ts">
@@ -24,18 +34,15 @@
 </script>
 
 <style scoped lang="scss">
-    h3 {
-    margin: 40px 0 0;
+    .deepskyblue {
+        color: deepskyblue;
     }
-    ul {
-    list-style-type: none;
-    padding: 0;
+
+    .button.is-info {
+        background-color: deepskyblue;
     }
-    li {
-    display: inline-block;
-    margin: 0 10px;
-    }
-    a {
-    color: #42b983;
+
+    .subtitle {
+        line-height: 26px;
     }
 </style>
