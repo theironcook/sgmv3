a953d1e910f12c794650e43eb6acb887361bc216
Andrii Mozhlianskyi <mozhlyanskiy1993@gmail.com>
2022-04-03 22:04:17 +0300

    remove extra dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import '@/style/index.scss';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
