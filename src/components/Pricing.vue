f1560220dd4b9c379f130e8731319e813d2b4ac2
Richard Wood <rich@saasglue.com>
2023-03-18 21:57:20 -0600

    pricing page update

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -3,14 +3,14 @@
         <div class="container">
             <div class="columns">
                 <div class="column">
-                    <h2 class="title is-size-2-desktop has-text-centered is-size-4-touch mb-5">Our Pricing</h2>
+                    <h2 class="title is-size-2-desktop has-text-centered is-size-4-touch mb-5">Start free, scale effortlessly</h2>
                 </div>
             </div>
             <div class="columns is-centered pricing-columns">
                 <div class="column is-narrow-desktop">
                     <div class="box content has-border-primary">
                         <header>
-                            <h5>Free</h5>
+                            <h5 class="has-text-centered">Free</h5>
                         </header>
                         <p class="has-text-weight-bold is-align-items-center is-flex is-justify-content-center">
                             <span class="pr-1">$</span>
@@ -24,11 +24,11 @@
                                 </span>
                                 <span>Run unlimited scripts from the web console</span>
                             </p>
-                            <p class="icon-text">
+                            <p class="icon-text is-flex is-align-items-center is-flex-wrap-nowrap">
                                 <span class="icon mr-2">
                                     <img src="@/assets/images/checkmark.svg" />
                                 </span>
-                                <span>Create unlimited agents</span>
+                                <span>First 100 job scripts per month free</span>
                             </p>
                             <p class="icon-text">
                                 <span class="icon mr-2">
@@ -36,11 +36,23 @@
                                 </span>
                                 <span>Store unlimited scripts</span>
                             </p>
-                            <p class="icon-text has-text-grey-lighter">
+                            <p class="icon-text">
                                 <span class="icon mr-2">
-                                    <img class="has-icon-muted" src="@/assets/images/checkmark.svg" />
+                                    <img src="@/assets/images/checkmark.svg" />
                                 </span>
                                 <span>Create unlimited jobs</span>
+                            </p>
+                            <p class="icon-text">
+                                <span class="icon mr-2">
+                                    <img src="@/assets/images/checkmark.svg" />
+                                </span>
+                                <span>Launch jobs manually</span>
+                            </p>
+                            <p class="icon-text">
+                                <span class="icon mr-2">
+                                    <img src="@/assets/images/checkmark.svg" />
+                                </span>
+                                <span>Install up to 10 agents</span>
                             </p>
                             <p class="icon-text has-text-grey-lighter">
                                 <span class="icon mr-2">
@@ -48,23 +60,17 @@
                                 </span>
                                 <span>Schedule jobs</span>
                             </p>
-                            <p class="icon-text has-text-grey-lighter">
-                                <span class="icon mr-2">
-                                    <img class="has-icon-muted" src="@/assets/images/checkmark.svg" />
-                                </span>
-                                <span>Launch jobs through the API</span>
-                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="column is-narrow-desktop">
                     <div class="box content has-border-primary">
                         <header>
-                            <h5>Standard</h5>
+                            <h5 class="has-text-centered">Standard</h5>
                         </header>
                         <p class="has-text-weight-bold is-align-items-center is-flex is-justify-content-center">
                             <span class="pr-1">$</span>
-                            <span class="is-size-1">0.01</span>
+                            <span class="is-size-1">0.001</span>
                             <span class="pt-5">/script</span>
                         </p>
                         <a href="https://console.saasglue.com/" class="button is-fullwidth is-primary has-text-weight-bold">GET STARTED</a>
@@ -75,11 +81,11 @@
                                 </span>
                                 <span>Run unlimited scripts from the web console</span>
                             </p>
-                            <p class="icon-text">
+                            <p class="icon-text is-flex is-align-items-center is-flex-wrap-nowrap">
                                 <span class="icon mr-2">
                                     <img src="@/assets/images/checkmark.svg" />
                                 </span>
-                                <span>Create unlimited agents</span>
+                                <span>First 100 job scripts per month free</span>
                             </p>
                             <p class="icon-text">
                                 <span class="icon mr-2">
@@ -97,17 +103,46 @@
                                 <span class="icon mr-2">
                                     <img src="@/assets/images/checkmark.svg" />
                                 </span>
-                                <span>Schedule jobs</span>
+                                <span>Launch jobs manually</span>
                             </p>
                             <p class="icon-text">
                                 <span class="icon mr-2">
                                     <img src="@/assets/images/checkmark.svg" />
                                 </span>
-                                <span>Launch jobs through the API</span>
+                                <span>Install unlimited agents</span>
+                            </p>
+                            <p class="icon-text">
+                                <span class="icon mr-2">
+                                    <img src="@/assets/images/checkmark.svg" />
+                                </span>
+                                <span>Schedule jobs</span>
                             </p>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="container content mt-5">
+            <div class="columns">
+                <div class="column is-2 is-offset-0">
+                </div>
+                <article class="column is-7 px-5">
+                    <h1 class="subtitle is-size-5">Usage pricing</h1>
+                    <p>With SaaSGlue you can create and run multi-task jobs with ease. Scripts that run within jobs are billed at a rate of $0.0001 per script. The first 100 job scripts per month are free. You can also run unlimited individual scripts through our web console for free.</p>
+
+                    <h2 class="subtitle is-size-5">AWS Lambda</h2>
+                    <p>If you prefer to run your scripts on AWS Lambda, we've got you covered. We charge an additional $0.0000166667 for every GB-second and $0.20 per 1M requests for scripts executed on AWS Lambda. This includes indvidual scripts executed on AWS Lambda via the web console.</p>
+
+                    <h2 class="subtitle is-size-5">Artifacts</h2>
+                    <p>We also provide the convenience of uploading and attaching artifacts to your tasks, which are downloaded and available to your scripts at runtime. Artifacts uploaded to SaaSGlue are charged at a rate of $0.09 per GB per month, while artifacts downloaded to execute a task are charged at a rate of $0.023 per GB.</p>
+
+                    <h2 class="subtitle is-size-5">Try SaaSGlue Today</h2>
+                    <p>At SaaSGlue, we believe in transparency and simplicity when it comes to pricing. So you can focus on what matters most - automating your software, while leaving the rest to us.</p>
+
+                    <div class="buttons">
+                        <a href="https://console.saasglue.com/" class="button is-primary has-action-shadow hax-text-weight-bold">GET STARTED</a>
+                    </div>
+                </article>
             </div>
         </div>
     </section>
